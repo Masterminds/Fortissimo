4bf1f7bfe2815100837424f16005764656b230ca
Matt Butcher <technosophos@gmail.com>
2010-11-22 14:06:36 -0600

    Added license block to docs.

--- Fortissimo/skel/src/Fortissimo.php
+++ Fortissimo/skel/src/Fortissimo.php
@@ -3608,4 +3608,27 @@ class FortissimoRequestMapper {
   * - Include Paths: By default, Fortissimo searches the includes/ directory for your source code.
   *  Sometimes you will want it to search elsewhere. Use include paths to add new locations for
   *  Fortissimo to search. This is done with Config::includePath().
+  *
+  * @section fortissimo_license License
+  Fortissimo
+  Matt Butcher <mbutcher@aleph-null.tv>
+  Copyright (C) 2009, 2010 Matt Butcher
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
   */
