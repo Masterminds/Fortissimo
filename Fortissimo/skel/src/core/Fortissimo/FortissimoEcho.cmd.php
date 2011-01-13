1714d2e47abc49d61d5d0943a5c28f1a90d69738
Matt Butcher <technosophos@gmail.com>
2011-01-13 12:51:22 -0600

    Added support for HTTP headers to FortissimoEchoCommand.

--- Fortissimo/skel/src/core/Fortissimo/FortissimoEcho.cmd.php
+++ Fortissimo/skel/src/core/Fortissimo/FortissimoEcho.cmd.php
@@ -15,6 +15,7 @@ class FortissimoEcho extends BaseFortissimoCommand {
       ->description('Echo the contents of the "text" parameter to standard output.')
       ->usesParam('type', 'The MIME type of the message, e.g. text/plain, text/html, application/javascript')
       ->withFilter('string')
+      ->usesParam('headers', 'Other HTTP headers to set. This should be an indexed array of header strings, e.g. array("Location: http://example.com").')
       ->usesParam('text', 'The text to echo.')
       //->withFilter('string')
       ;
@@ -23,9 +24,14 @@ class FortissimoEcho extends BaseFortissimoCommand {
   public function doCommand() {
     
     $type = $this->param('type', NULL);
+    $headers = $this->param('headers', array());
     
     if (!empty($type)) {
       header('Content-Type: ' . $type);
+    }
+    
+    foreach ($headers as $header) {
+      header($header);
     }
     
     print $this->parameters['text'];
