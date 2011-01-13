f44f475bfb434dd2b5a521c9df71193091d7b277
Matt Butcher <technosophos@gmail.com>
2011-01-13 12:30:04 -0600

    Added support for MIME type to FortissimoEcho.

--- Fortissimo/skel/src/core/Fortissimo/FortissimoEcho.cmd.php
+++ Fortissimo/skel/src/core/Fortissimo/FortissimoEcho.cmd.php
@@ -13,12 +13,21 @@ class FortissimoEcho extends BaseFortissimoCommand {
   public function expects() {
     return $this
       ->description('Echo the contents of the "text" parameter to standard output.')
+      ->usesParam('type', 'The MIME type of the message, e.g. text/plain, text/html, application/javascript')
+      ->withFilter('string')
       ->usesParam('text', 'The text to echo.')
       //->withFilter('string')
       ;
   }
   
   public function doCommand() {
+    
+    $type = $this->getParam('type', NULL);
+    
+    if (!empty($type)) {
+      header('Content-Type: ' . $type);
+    }
+    
     print $this->parameters['text'];
   }
 }
