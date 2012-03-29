6a0d5c47744a17a453219316ba5fc209e612a9a0
Matt Butcher <technosophos@gmail.com>
2012-03-29 09:14:39 -0600

    Updated Memcache, but not tested.

--- src/Fortissimo/Cache/Memcache.php
+++ src/Fortissimo/Cache/Memcache.php
@@ -2,10 +2,10 @@
 /** @file
  * This file contains the definition of a memcache backend for a FortissimoRequestCache.
  */
- 
+namespace Fortissimo\Cache;
 /**
  * Provide access to one or more memcache servers acting as a caching cluster.
- * 
+ *
  * This uses pecl/memcache for access to Memcached servers. This does not connect to servers
  * until absolutely necessary. It is designed to be used as a FortissimoRequestCache instance.
  *
@@ -21,59 +21,53 @@
  *  - isDefault: As with all caches, you can mark this cache as the default by setting the 
  *    isDefault param to TRUE. Only one cache can be set to be the default.
  *
- *  Example: 
- *  
+ *  Example:
+ *
  * @code
  * <?php
- * Config::cache('memcache')
- *   ->whichInvokes('FortissimoMemcacheCache')
- *   ->withParam('servers')
- *     ->whoseValueIs(array('example.com:11211', 'example.com:11212'))
- *   ->withParam('persistent')
- *     ->whoseValueIs(FALSE)
- *   ->withParam('compress')
- *     ->whoseValueIs(FALSE)
- *
- *   ->withParam('isDefault')
- *     ->whoseValueIs(TRUE)
+ * $register->cache('\Fortissimo\Cache\Memcache', 'memcache')
+ *   ->using('servers', array('example.com:11211', 'example.com:11212'))
+ *   ->using('persistent', FALSE)
+ *   ->using('compress', TRUE)
+ *   ->using('isDefault', TRUE)
  * ;
  * ?>
  * @endcode
  *
  * @ingroup Fortissimo
  */
-class FortissimoMemcacheCache extends FortissimoCache {
+class Memcache extends Base {
   protected $memcache = NULL;
   protected $compress = FALSE;
-  
+
   /**
    * Initialize access to a memcached cluster.
    *
    */
   public function init() {
-    
+
     if (empty($this->params['server'])) {
-       throw FortissimoInterruptException('No memcache server was specified, but init was attempted.');
+       throw \Fortissimo\InterruptException('No memcache server was specified, but init was attempted.');
     }
-    
-    $this->memcache = new Memcache();
+
+    $this->memcache = new \Memcache();
     $this->compress = isset($this->params['compress']) 
       && filter_var($this->params['compress'], FILTER_VALIDATE_BOOLEAN);
-    
+
     $servers = $this->params['server'];
     $persist = isset($this->params['persistent']) 
       && filter_var($this->params['persistent'], FILTER_VALIDATE_BOOLEAN);
-    
+
     if (is_string($servers)) {
       $servers = array($servers);
     }
-    
+
     foreach ($servers as $server) {
       list($name, $port) = explode(':', $server, 2);
       $this->memcache->addServer($name, $port, $persist);
     }
   }
-  
+
   public function clear() {
     $this->memcache->flush();
   }
