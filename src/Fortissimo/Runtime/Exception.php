dd38384db7d485af7a0237c16850c95cfcab6878
Matt Butcher <technosophos@gmail.com>
2012-03-29 08:51:01 -0600

    Added runtime exception.
    This captures errors that occur in the runtime before a chain
    is executed.

---
+++ src/Fortissimo/Runtime/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ *
+ * Runtime exception.
+ */
+namespace Fortissimo\Runtime;
+
+/**
+ * The Runtime Exception
+ *
+ * A fatal exception thrown when the runtime is misconfigured.
+ */
+class Exception extends \Fortissimo\InterruptException {}
