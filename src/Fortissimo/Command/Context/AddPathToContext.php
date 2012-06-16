bd377a1fdf97c1df4e38dc35836d37b0f5dfa3ae
Technosophos <technosophos@gmail.com>
2012-06-16 07:35:08 -0500

    Added new AddPathToContext command.

---
+++ src/Fortissimo/Command/Context/AddPathToContext.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @file
+ * Parse a path and add the components to the context.
+ */
+namespace Fortissimo\Command\Context;
+/**
+ * Parse a path and add its path components to the context.
+ * @since 2.0.0
+ *
+ * For example, this can take  foo/bar/123 and a description of 
+ * the parameters it expects, and parse it into an
+ * associative array:
+ *
+ * @code
+ * <?php
+ *
+ * $registry->route('someRoute')
+ *   ->does('\Fortissimo\Command\Context\AddPathToContext', 'c')
+ *   ->using('path', 'foo/bar/123')
+ *   ->using('template', '%s/%s/%d')
+ *   ->using('names', array('name', 'type', 'record_id'))
+ * ;
+ * ?>
+ *
+ * Names are applied in order of match. So 'name' above will be assigned 
+ * to the match of the first '%s', which will be 'foo'.
+ *
+ * Running the route will add the following into the context:
+ *
+ * @code
+ * <?php
+ *  array(
+ *    'name' => 'foo',
+ *    'type' => 'bar',
+ *    'record_id' => 123,
+ *  );
+ * ?>
+ * @endcode
+ */
+class AddPathToContext extends \Fortissimo\Command\Base {
+  const RSEP = "\t";
+  public function expects() {
+    return $this->description('Parse a path and store the path parts as named context items.')
+      ->usesParam('path', 'The path to parse')->whichIsRequired()
+      ->usesParam('template', 'A template of what the entry is expected to look like')
+        ->whichIsRequired()
+      ->usesParam('names', 'An array of names to which these values will be assigned.')
+    ;
+  }
+
+  public function doCommand() {
+    $path = $this->param('path');
+    $template = $this->param('template');
+    $names = $this->param('names');
+
+    $res = $this->scan($path, $template, $names);
+
+    $this->context->addAll($res);
+  }
+
+  public function scan($path, $template, $names) {
+    $path = strtr($path, '/', self::RSEP);
+    $template = strtr($template, '/', self::RSEP);
+    $matches = sscanf($path, $template);
+
+    // array_combine is not robust enough to handle
+    // sscanf failures.
+    $count = count($matches);
+    $add = array();
+    for ($i = 0; $i < $count; ++$i) {
+      $add[$names[$i]] = $matches[$i];
+    }
+
+    return $add;
+
+  }
+}
