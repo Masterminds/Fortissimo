49e1b77e582696a7cb3ae40a68a17950ffbad50d
Technosophos <technosophos@gmail.com>
2012-06-26 13:55:52 -0500

    Added IntoArray command.

---
+++ src/Fortissimo/Command/Context/IntoArray.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ * Put some or all of the context into an array, which is then placed 
+ * back into the context.
+ */
+namespace Fortissimo\Command\Context;
+/**
+ * Put some or all of a context into an associative array.
+ *
+ * This makes it possible to build an array based on the values in the
+ * context. The resulting array will then be put back into the context.
+ *
+ * Optionally, you may specify which context items should be placed into
+ * the array. This will then return an array with as many items as it
+ * can find.
+ * @since 2.0.0
+ */
+class IntoArray extends \Fortissimo\Command\Base {
+  public function expects() {
+    return $this->description('Load the conents of an INI file into the context.')
+      ->usesParam('names', 'The names of context items that should be put into this array if found.')
+      ->andReturns('An associative array of context names/values.')
+    ;
+  }
+
+  public function doCommand() {
+    $names = $this->param('names', NULL);
+
+    if (is_array($names)) {
+      $buffer = array();
+      foreach ($names as $name) {
+        if ($this->context->has($name)) {
+          $buffer[$name] = $this->context->get($name);
+        }
+      }
+    }
+    else {
+      $buffer = $this->context->toArray();
+    }
+
+    return $buffer;
+  }
+}
