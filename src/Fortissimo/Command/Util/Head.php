d690ceeecbdfd7e79d7db27e5100742039562ca0
Technosophos <technosophos@gmail.com>
2012-07-09 12:17:47 -0500

    Added Head command.

---
+++ src/Fortissimo/Command/Util/Head.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * Provides the functional "head" operation.
+ */
+namespace Fortissimo\Command\Util;
+/**
+ * Retrieve the HEAD item of a list.
+ *
+ * In functional programming, the head is the first item in a list. This 
+ * puts the first item in a list into the context.
+ *
+ */
+class Increment extends \Fortissimo\Command\Base {
+  public function expects() {
+    return $this
+      ->description('Put the first (head) value into the context. This does not modify the list.')
+      ->usesParam('list', 'An iterable or an array.')
+      ->andReturns('The first item in the iterable.');
+  }
+  public function doCommand() {
+    $list = $this->param('list', array());
+    // XXX: Should this use a foreach? Not sure if a plain Iterable can 
+    // do current().
+    $ret = current($list);
+    return $ret;
+  }
+}
